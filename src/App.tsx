984414bcaf22b8a016f7268bbcc027f49415ce17
zephyrus <pandey.piyus@gmail.com>
2021-05-22 23:21:49 +0530

    adding 🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 function App() {
-  return <div>App</div>;
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+    console.log(service);
+  };
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 export default App;
