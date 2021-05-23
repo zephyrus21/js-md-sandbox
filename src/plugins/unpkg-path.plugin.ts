65ddecb070d89640122685a472c41bc62c4778cb
zephyrus <pandey.piyus@gmail.com>
2021-05-23 11:25:35 +0530

    refactoring pkg plugin 😁

--- src/plugins/unpkg-path.plugin.ts
+++ src/plugins/unpkg-path.plugin.ts
@@ -1,12 +1,29 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            path: new URL(args.path, args.importer + '/').href,
+            namespace: 'a',
+          };
+        }
+        return { path: `https://unpkg.com/${args.path}`, namespace: 'a' };
+
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //     namespace: 'a',
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +33,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'medium-test-pkg';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
         }
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
