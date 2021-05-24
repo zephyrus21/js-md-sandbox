682cc9893061ee751d4226d8d6e05f6c5b203814
zephyrus <pandey.piyus@gmail.com>
2021-05-24 14:02:36 +0530

    fiximg ts bus 🐞

--- src/plugins/unpkg-path.plugin.ts
+++ src/plugins/unpkg-path.plugin.ts
@@ -1,5 +1,8 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({ name: 'filecache' });
 
 export const unpkgPathPlugin = () => {
   return {
@@ -37,12 +40,27 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+
+        //! check to see if we have already fetched the file
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        //! if it is cached then return immediately
+        if (cachedResult) return cachedResult;
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        //! store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
