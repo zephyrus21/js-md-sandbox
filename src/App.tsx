5d1d158df6d47101479678174a7b8ac304de9000
zephyrus <pandey.piyus@gmail.com>
2021-05-24 20:04:27 +0530

    refactoring whole service code 🐱‍👤

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
--- src/plugins/fetch.plugin.ts
+++ src/plugins/fetch.plugin.ts
@@ -8,14 +8,16 @@ export const fetchPlugin = (input: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: input,
-          };
-        }
+      //! To load the main file of the module
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: input,
+        };
+      });
 
+      //! Function to get files from cached if available
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         //! check to see if we have already fetched the file
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
@@ -23,7 +25,37 @@ export const fetchPlugin = (input: string) => {
 
         //! if it is cached then return immediately
         if (cachedResult) return cachedResult;
+      });
 
+      //! Function for loading css files
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const cssFile = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${cssFile}';
+          document.head.appendChild(style); 
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        //! store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      //! Function for loading jsx files
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
