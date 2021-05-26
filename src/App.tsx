2d7c4c6c0f68bbdb79537b5f2195a509725bfb4a
zephyrus <pandey.piyus@gmail.com>
2021-05-26 19:07:17 +0530

    refactoring es-build

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin, fetchPlugin } from './plugins';
+import React, { useState } from 'react';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+import bundle from './bundler';
 
 function App() {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
+  const [code, setCode] = useState('');
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        // 'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(await bundle(input));
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try{
-              eval(event.data);
-            } catch (error) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>';
-              console.log(error);
-            } 
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
       <CodeEditor
         initialValue='// Hello World
 '
-
-
-
-
-
-
-
-
-
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      {/* <pre>{code}</pre> */}
-      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
+      <Preview code={code} />
     </div>
   );
 }
