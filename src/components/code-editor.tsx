fbc863daa67126ddb5cd9dc30aae159ea92d6b06
zephyrus <pandey.piyus@gmail.com>
2021-05-26 17:23:12 +0530

    adding editors options 🧩

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height='500px' />;
+  return (
+    <MonacoEditor
+      height='500px'
+      language='javascript'
+      theme='vs-dark'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
