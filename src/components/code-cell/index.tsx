96101e64f39a0bb63317de0063c05e0cba2ac869
zephyrus <pandey.piyus@gmail.com>
2021-05-28 20:59:05 +0530

    refactoring file structures

--- src/components/code-cell/index.tsx
+++ src/components/code-cell/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import CodeEditor from './code-editor';
-import Resizable from './resizable';
-import Preview from './preview';
-import bundle from '../bundler';
+import CodeEditor from '../code-editor';
+import Resizable from '../resizable';
+import Preview from '../preview';
+import bundle from '../../bundler';
 
 function CodeCell() {
   const [input, setInput] = useState('');
